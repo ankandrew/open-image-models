91d24bc611376d757b688c6586810ba2fb8af66a
ankandrew <61120139+ankandrew@users.noreply.github.com>
2024-09-28 12:59:59 -0300

    Cleanup YOLOv9 preprocessing

--- open_image_models/detection/core/yolo_v9/preprocess.py
+++ open_image_models/detection/core/yolo_v9/preprocess.py
@@ -15,7 +15,7 @@ def preprocess(img: np.ndarray, img_size: tuple[int, int] | int):
     :return: Preprocessed image tensor, resize ratio, and padding (dw, dh).
     """
     # Resize the input image to match training format
-    im, ratio, (dw, dh) = letterbox(img, img_size, stride=32, auto=False, scale_fill=False)
+    im, ratio, (dw, dh) = letterbox(img, img_size)
     # HWC to CHW, BGR to RGB
     im = im.transpose((2, 0, 1))[::-1]
     # 0 - 255 to 0.0 - 1.0
@@ -28,48 +28,6 @@ def preprocess(img: np.ndarray, img_size: tuple[int, int] | int):
 
 
 def letterbox(
-    im: np.ndarray,
-    new_shape: tuple[int, int] | int = (640, 640),
-    color: tuple[int, int, int] = (114, 114, 114),
-    auto: bool = True,
-    scale_fill: bool = False,
-    scaleup: bool = True,
-    stride: int = 32,
-):
-    """
-    Taken from `ultralytics.utils.augmentations.letterbox`
-    """
-    shape = im.shape[:2]  # current shape [height, width]
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
-
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better val mAP)
-        r = min(r, 1.0)
-
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
-    elif scale_fill:  # stretch
-        dw, dh = 0.0, 0.0
-        new_unpad = (new_shape[1], new_shape[0])
-        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
-        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return im, ratio, (dw, dh)
-
-
-# TODO: Compare with letterbox
-def letterbox2(
     im: np.ndarray,
     new_shape: tuple[int, int] | int = (640, 640),
     color: tuple[int, int, int] = (114, 114, 114),
